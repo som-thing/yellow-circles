f047c8f7cc7026daaffa1a54fb30509ad88c34f7
som-thing <81899399+som-thing@users.noreply.github.com>
2022-11-11 19:13:20 +0600

    ye circles

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Окружность")
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 255))
+        rad = random.randint(3, 100)
+        qp.drawEllipse(QPoint(210, 200), rad, rad)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+
